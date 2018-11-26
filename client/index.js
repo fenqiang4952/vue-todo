fda04b5a2dbcde68c81df210269af1bb8294b237
fenqiang4952 <495246490@qq.com>
2018-11-27 00:34:01 +0800

    add router

--- client/index.js
+++ client/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './app.vue'
+import CreateRouter from './config/router'
 
 import './assets/styles/global.styl'
 
 const root = document.createElement('div')
 document.body.appendChild(root)
 
+const router = CreateRouter()
+
 new Vue({
+  router,
   render: (h) => h(App)
 }).$mount(root)
